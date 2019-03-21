0e83e1c0b203645052bd2d4fbd26b6cb84789618
Rezwanul-Haque <rezwanul.cse@gmail.com>
2019-03-21 18:41:05 +0600

    Initial commit

---
+++ blog/app.py
@@ -0,0 +1,13 @@
+blogs = dict() # blog_name : Blog object
+
+def menu():
+    # Show the user the available blogs
+    # Let the user make a choice
+    # Do something with that choice
+    # Eventually exit
+
+    print_blogs()
+
+def print_blogs():
+    for key, blog in blogs.items():  # [(blog_name, Blog), (blog_name, Blog)]
+        print("- {}".format(blog))
---
+++ blog/tests/integration/__init__.py
@@ -0,0 +1,2 @@
+"""These tests are done when two or more models are interacting with each other or depending on each other.
+"""
